fa0336d9f1c3af4ccb301520124cef68ef03d324
LordXor <LordXor@Imac-de-Oscar.local>
2025-03-11 09:38:17 +0100

    Added copilot and redefined some hot-keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,8 @@
 -- Add any additional keymaps here
 --
 --
-vim.api.nvim_set_keymap("n", "<C-b>", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-d>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-i>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-v>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-t>', ':lua require("dap.repl").open()<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "<A-b>", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-d>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-i>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-v>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-t>", ':lua require("dap.repl").open()<CR>', { noremap = true, silent = true })
